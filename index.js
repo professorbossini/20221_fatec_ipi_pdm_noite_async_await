b7615b16da7c514a433729e875376bd1d7c8f448
Rodrigo Bossini <professor@gmail.com>
2022-03-18 20:30:53 -0300

    Consumo do WS com promises encadeadas. Exercicio de exibicao de temp min, max e descricao proposto e resolvido

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 require('dotenv').config()
 const axios = require ('axios')
 
-const { APPID, PROTOCOL , BASE_URL, UNITS, LANGUAGE } = process.env
+const { APPID, PROTOCOL , BASE_URL, UNITS, LANGUAGE, CNT, Q } = process.env
 
-const url = `${PROTOCOL}://${BASE_URL}?q=Itu&appid=${APPID}&lang=${LANGUAGE}&units=${UNITS}`
+const url = `${PROTOCOL}://${BASE_URL}?q=${Q}&appid=${APPID}&lang=${LANGUAGE}&units=${UNITS}&cnt=${CNT}`
 
 
 axios.get(url)
-.then(res => console.log(res.data))
+.then(res => {
+    console.log(res.data)
+    return res.data
+})
+.then(res => {
+    console.log(res.cnt)
+    return res
+})
+.then(res => {
+    //return x.list
+    return res['list']
+})
+.then(res => {
+    res.forEach(elemento => {
+        console.log (new Date(+elemento.dt * 1000).toLocaleString())
+        console.log(`Temp min: ${elemento.main.temp_min}`)
+        console.log(`Temp max: ${elemento.main.temp_max}`)
+        console.log(`Descrição: ${elemento.weather[0].description}`)
+    })
+    // for (let i = 0; i < res.length; i++){
+    //     console.log (new Date(+(res[i].dt) * 1000).toLocaleString())
+    //     console.log ("Temp min: " + res[i].main.temp_min)
+    //     console.log("Temp max: " + res[i]['main']['temp_max'])
+    //     console.log ("Descrição: " + res[i].weather[0].description)
+    // }
+    //para cada previsão na lista
+    //exibir a temperatura minima
+    //exibir a temperatura maxima
+    //exibir a descricao
+})
