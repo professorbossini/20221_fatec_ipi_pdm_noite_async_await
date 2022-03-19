5ab56d6d060f67f7c5d8bee71f3021296d6f8a15
Rodrigo Bossini <professor@gmail.com>
2022-03-18 21:19:40 -0300

    Exemplo com async

--- index.js
+++ index.js
@@ -1,39 +1,71 @@
-require('dotenv').config()
-const axios = require ('axios')
+//Promises: ECMAScript 2015
+//async/await: ECMAScript 2017
 
-const { APPID, PROTOCOL , BASE_URL, UNITS, LANGUAGE, CNT, Q } = process.env
-
-const url = `${PROTOCOL}://${BASE_URL}?q=${Q}&appid=${APPID}&lang=${LANGUAGE}&units=${UNITS}&cnt=${CNT}`
-
-
-axios.get(url)
-.then(res => {
-    console.log(res.data)
-    return res.data
-})
-.then(res => {
-    console.log(res.cnt)
-    return res
-})
-.then(res => {
-    //return x.list
-    return res['list']
-})
-.then(res => {
-    res.forEach(elemento => {
-        console.log (new Date(+elemento.dt * 1000).toLocaleString())
-        console.log(`Temp min: ${elemento.main.temp_min}`)
-        console.log(`Temp max: ${elemento.main.temp_max}`)
-        console.log(`Descrição: ${elemento.weather[0].description}`)
-    })
-    // for (let i = 0; i < res.length; i++){
-    //     console.log (new Date(+(res[i].dt) * 1000).toLocaleString())
-    //     console.log ("Temp min: " + res[i].main.temp_min)
-    //     console.log("Temp max: " + res[i]['main']['temp_max'])
-    //     console.log ("Descrição: " + res[i].weather[0].description)
+// async function hello (nome){
+    //     return `Hello, ${nome}`
     // }
-    //para cada previsão na lista
-    //exibir a temperatura minima
-    //exibir a temperatura maxima
-    //exibir a descricao
-})
+const hello = async (nome) => {
+    return `Hello ${nome}`
+}
+const resultado = hello("Ana")
+resultado.then (r => console.log(r))
+
+
+// function hello (nome){
+//     return new Promise (function(resolve, reject){
+//         resolve(`Hello, ${nome}`)
+//     })
+// }
+// const resultado = hello ("Pedro")
+// resultado.then (res => console.log(res))
+
+// function hello (nome){
+//     return `Hello, ${nome}`
+// }
+// const resultado = hello ("João")
+// console.log(resultado)
+
+// // require('dotenv').config()
+// // const axios = require ('axios')
+
+// // const { axios } = require("axios")
+
+// // const { APPID, PROTOCOL , BASE_URL, UNITS, LANGUAGE, CNT, Q } = process.env
+
+// // const url = `${PROTOCOL}://${BASE_URL}?q=${Q}&appid=${APPID}&lang=${LANGUAGE}&units=${UNITS}&cnt=${CNT}`
+
+
+// axios.get(url)
+// .then(res => {
+//     console.log(res.data)
+//     return res.data
+// })
+// .then(res => {
+//     console.log(res.cnt)
+//     axios.get(url).then(r => {
+
+//     })
+//     return res
+// })
+// .then(res => {
+//     //return x.list
+//     return res['list']
+// })
+// .then(res => {
+//     res.forEach(elemento => {
+//         console.log (new Date(+elemento.dt * 1000).toLocaleString())
+//         console.log(`Temp min: ${elemento.main.temp_min}`)
+//         console.log(`Temp max: ${elemento.main.temp_max}`)
+//         console.log(`Descrição: ${elemento.weather[0].description}`)
+//     })
+//     // for (let i = 0; i < res.length; i++){
+//     //     console.log (new Date(+(res[i].dt) * 1000).toLocaleString())
+//     //     console.log ("Temp min: " + res[i].main.temp_min)
+//     //     console.log("Temp max: " + res[i]['main']['temp_max'])
+//     //     console.log ("Descrição: " + res[i].weather[0].description)
+//     // }
+//     //para cada previsão na lista
+//     //exibir a temperatura minima
+//     //exibir a temperatura maxima
+//     //exibir a descricao
+// })
